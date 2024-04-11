f4a369a5f0b4602a68e3da836e7b0628c4ae32de
Kristofer Rogue <75965259+Kristofer-Rogue@users.noreply.github.com>
2024-04-11 10:14:04 +0700

    Added getting all element parameters

--- PropertiesShower/Commands/PropertiesShower.cs
+++ PropertiesShower/Commands/PropertiesShower.cs
@@ -1,6 +1,9 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Linq;
+using System.Text;
 
 namespace PropertiesShower.Commands
 {
@@ -10,8 +13,54 @@ namespace PropertiesShower.Commands
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            TaskDialog.Show("PropertiesShower", "Получилось!");
+            var uidoc = commandData.Application.ActiveUIDocument;
+            var doc = uidoc.Document;
+            var choices = uidoc.Selection;
+
+            var hasPickOne = choices.PickObject(ObjectType.Element, "Выберите элемент");
+            if (hasPickOne == null)
+            {
+                return Result.Succeeded;
+            }
+
+            var selectedElement = doc.GetElement(hasPickOne.ElementId);
+
+            var listParameters = GetElementParameters(selectedElement);
+
+            TaskDialog.Show(selectedElement.Name, listParameters);
+
             return Result.Succeeded;
+        }
+
+        private string GetElementParameters(Element element)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var p in element.Parameters.Cast<Parameter>())
+            {
+                var parameterValue = GetParameterValue(p);
+                if (parameterValue != null)
+                {
+                    stringBuilder.AppendLine($"{p.Definition.Name}: {parameterValue}");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+        private string GetParameterValue(Parameter parameter)
+        {
+            switch (parameter.StorageType)
+            {
+                case StorageType.Double:
+                    return parameter.AsDouble().ToString();
+                case StorageType.Integer:
+                    return parameter.AsInteger().ToString();
+                case StorageType.String:
+                    return parameter.AsString();
+                case StorageType.ElementId:
+                    return parameter.AsElementId().IntegerValue.ToString();
+                default:
+                    return "<Unsupported Type>";
+            }
         }
     }
 }
