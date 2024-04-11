d47c4b03d6c821e7d12bf9e4b8b072d72a9f371a
Kristofer Rogue <75965259+Kristofer-Rogue@users.noreply.github.com>
2024-04-11 08:45:06 +0700

    Integrated the command into Revit

---
+++ PropertiesShower/Commands/PropertiesShower.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace PropertiesShower.Commands
+{
+    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.Manual)]
+    internal class PropertiesShower : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("PropertiesShower", "Получилось!");
+            return Result.Succeeded;
+        }
+    }
+}
